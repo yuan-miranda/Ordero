05b924111259b0aa489b17728af6d11f517ddcca
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-04-21 15:40:18 +0800

    made left

--- your_orders.php
+++ your_orders.php
@@ -151,7 +151,7 @@ if (empty($_SESSION['user_id'])) {
 					<button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse"
 						data-target="#mainNavbarCollapse">&#9776;</button>
 					<!-- <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/ordero_icon.svg" alt=""> </a> -->
-					<div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+					<div class="collapse navbar-toggleable-md  float-lg-left" id="mainNavbarCollapse">
 						<ul class="nav navbar-nav">
 							<li class="nav-item"> <a class="nav-link active" href="index.php">Home <span
 										class="sr-only">(current)</span></a> </li>
